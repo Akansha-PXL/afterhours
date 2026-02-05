da0902a6fd40ee8e62302c937425c345518539c3
Akansha-PXL <akanshakrishnan@gmail.com>
2026-02-05 12:32:11 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+"use strict";
+
+const video = document.getElementById("bg-video");
+const changeBtn = document.getElementById("change-bg");
+const aboutBtn = document.getElementById("about-btn");
+const closeAbout = document.getElementById("close-about");
+const modal = document.getElementById("about-modal");
+
+const videos = [
+    "assets/videos/cafe.mp4",
+    "assets/videos/rain.mp4",
+    "assets/videos/city.mp4"
+];
+
+let current = 0;
+
+// Change background video
+changeBtn.addEventListener("click", () => {
+    current = (current + 1) % videos.length;
+    video.src = videos[current];
+    video.play();
+});
+
+// About modal
+aboutBtn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+});
+
+closeAbout.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
 
